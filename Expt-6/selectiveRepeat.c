4d0d88f46d57e661457c56ebb48194f6e022fe70
Shefa <133137439+Shefaoudeen@users.noreply.github.com>
2024-05-05 21:11:21 +0530

    Create selectiveRepeat.c

---
+++ Expt-6/selectiveRepeat.c
@@ -0,0 +1,34 @@
+#include<stdio.h> 
+#include<time.h> 
+#include<stdlib.h> 
+int main() { 
+ int nf,N,no=0,i=1; 
+ srand(time(NULL)); 
+ printf("\nEnter the no of frames : "); 
+ scanf("%d",&nf); 
+ printf("\nEnter window size : "); 
+ scanf("%d",&N); 
+ while(i<=nf) { 
+  int x=0; 
+  for(int j=i;j<i+N && j<=nf;j++) { 
+   printf("\nSent frame : %d",j); 
+   no++; 
+  } 
+ for(int j=i;j<i+N && j<=nf;j++) { 
+   int flag = rand()%2; 
+   if(!flag) { 
+    printf("\nACK for frame : %d",j); 
+    x++; 
+ } 
+   else { 
+    printf("\nFrame %d not received",j); 
+    printf("\nRetransmitting the frame %d",j--); 
+    no++; 
+  } 
+ } 
+ printf("\n"); 
+ i+=x; 
+} 
+printf("\nTotal no of transmissions : %d\n\n",no); 
+return 0; 
+}
